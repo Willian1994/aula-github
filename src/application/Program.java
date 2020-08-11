f156f8c5d9cff52c784bc8789cd10c2489430cf3
Willian <willianbuonaduce@gmail.com>
2020-08-11 20:46:24 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+ 		
+		System.out.println("How many employees will be registered ? ");
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++) {
+			
+			System.out.println("Employoee #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);
+			
+			
+		}
+		
+		System.out.println("Enter the employee id that will have salary increase : ");
+		int idsalary = sc.nextInt();
+		Integer pos = position(list, idsalary);
+		if (pos == null) {
+			System.out.println("Thsi id does not exist! ");
+		}
+		else {
+			System.out.println(("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSlary(percent);
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+	public int position(List<Employee> list, int id) {
+		for (int i = 0; i < list.size(); i++ ) {
+			if (list.get(i).getId() == id)
+				return i;
+		}
+	}
+	
+	return null;
+}
